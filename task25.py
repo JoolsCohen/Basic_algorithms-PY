1925d93dbffa1c62886df989d73d7838d416cc46
«Julie» <joolescohen@gmail.com>
2023-05-30 22:39:06 +0300

    Done

--- task25.py
+++ task25.py
@@ -10,12 +10,38 @@ Output: a_0 a_1 a_2 b_0 c_0 a_3 a_4 d c_1 d_1 d_2
 Для решения данной задачи используйте функцию split()
 '''
 
-line = input('Введите символы строки через пробел: ').split() # создаем список, пользователь вводит
-res = {}                                                      # создаем словарь
-for i in line:                                                # циклом по списку
-    if i in res:                                              # заполняем словарь, где символ=ключ
-        print(f'{i}_{res[i]}', end='  ')
+s = 'a a a b c a a d c d d'
+s = s.split()
+print(s)
+final_string = ''
+for i in range(len(s)):
+    if s[0:i].count(s[i]) == 0:
+        final_string += f' {s[i]}'
     else:
-        print(f'{i}_{0}', end='  ')
-    res[i] = res.get(1, 0)+1
-#print(res)                               {'d': 1, 'f': 1, 's': 1, 'g': 1, 'e': 1, 't': 1, 'l': 1}
+        final_string += f' {s[i]}_{s[0:i].count(s[i])}'
+print(final_string)
+
+
+# word = input("Введите строку: ").split()
+# result = {}
+# for i in word:
+#     if i in result:
+#         print(f'{i}_{result[i]}', end=' ')
+#         result[i] += 1
+#     else:
+#         print(i, end=' ')
+#         result[i] = 1
+
+
+
+
+
+# line = input('Введите символы строки через пробел: ').split() # создаем список, пользователь вводит
+# res = {}                                                      # создаем словарь
+# for i in line:                                                # циклом по списку
+#     if i in res:                                              # заполняем словарь, где символ=ключ
+#         print(f'{i}_{res[i]}', end='  ')
+#     else:
+#         print(f'{i}_{0}', end='  ')
+#     res[i] = res.get(1, 0)+1
+# # print(res) {'d': 1, 'f': 1, 's': 1, 'g': 1, 'e': 1, 't': 1, 'l': 1}
--- task27.py
+++ task27.py
@@ -1,32 +1,34 @@
 '''
-    Пользователь вводит текст(строка). Словом считается последовательность непробельных
-символов идущих подряд, слова разделены одним или большим числом пробелов или символами
-конца строки.
-    Определите, сколько различных слов содержится в этом тексте.
-
-Input:
-She sells sea shells on the sea shore; The shells that she sells are sea shells I'm sure.
-So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells.
-
-Output: 17
-
-1 вариант решения через множество
-
-line = (input('Введите символы строки через пробел: ').lower().split())  # вводим строку,
-# lower переводит символы в нижний регистр, split разделят список по пробелам (по умолчанию)
-words = set()         # создаем пустое множенство
-for i in line:
-    words.add(i)    # заполняем множество уникальными словами
-print(len(words))   # выводим количество элементов в множестве
-
-
-2 вариант решения, записанное в одну строку
-
-
-
-
-3 вариант - решение через словарь
+Пользователь вводит текст(строка). Словом считается
+последовательность непробельных символов идущих
+подряд, слова разделены одним или большим числом
+пробелов. Определите, сколько различных слов
+содержится в этом тексте.
+Input: She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure.So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells
+Output: 13
 '''
+
+# 1 вариант решения через множество
+
+# line = (input('Введите символы строки через пробел: ').lower().split())  # вводим строку,
+# # lower переводит символы в нижний регистр, split разделят список по пробелам (по умолчанию)
+# words = set()         # создаем пустое множенство
+# for i in line:
+#     words.add(i)    # заполняем множество уникальными словами
+# print(len(words))   # выводим количество элементов в множестве
+
+
+# 2 вариант решения, записанное в одну строку
+
+# print(len(set(input('Введите текст ').upper().split())))
+
+print(len(set(input('Введите текст ').upper().replace('.', ' ').split())))
+
+
+# 3 вариант - решение через словарь
 
 # list = input('Введите текст: ').split()
 # dict = {}
@@ -34,6 +36,3 @@ print(len(words))   # выводим количество элементов в 
 #     dict[i.lower()]=0
 # print(len(dict.keys()))
 
-print(len(set(input('Введите текст ').lower().split())))
-
-# print(len(set(input('Введите текст ').upper().replace('.', ' ').split())))
