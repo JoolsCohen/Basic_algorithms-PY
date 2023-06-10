75fdbb0215030eb480f48479ef6c854b65b71773
«Julie» <joolescohen@gmail.com>
2023-06-10 14:38:52 +0300

    Done

--- task36.py
+++ task36.py
@@ -11,13 +11,13 @@
 print_operation_table(lambda x, y: x * y)
 
 Вывод:
-1  2  3   4   5   6
-2  4  6   8   10  12
-3  6  9   12  15  18
-4  8  12  16  20  24
-5 10  15  20  25  30
-6 12  18  24  30  36
+1 2 3 4 5 6
 
+2 4 6 8 10 12
+3 6 9 12 15 18
+4 8 12 16 20 24
+5 10 15 20 25 30
+6 12 18 24 30 36
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
@@ -34,11 +34,12 @@ print_operation_table(lambda x, y: x * y)
 '''
 
 
-def print_operation_table(operation, num_rows=9, num_columns=9):
+def print_operation_table(operation, num_rows=6, num_columns=6):
     if num_rows < 2 or num_columns < 2:
         print('ОЩИБКА! Размерности таблицы должны быть больше 2!')
     else:  # '  ' разделитель между элементами
         print('\t'.join([str(i) for i in range(1, num_columns + 1)]))  # первая строка просто заполняется 1,2,3,4,5,6
+        print()
         for i in range(2, num_rows + 1):
             print(i, end='\t')  # print(i, end = '\t') означает, что следующий i будет через 'таб'
             for j in range(2, num_columns + 1):
